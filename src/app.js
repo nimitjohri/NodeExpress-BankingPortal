e542365c6e1b50b3d9bfb5aa3268857689bb2822
nimitjohri <nimitjohri5@gmail.com>
2018-11-14 15:16:17 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const users = JSON.parse(userData);
 app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts}));
 
 app.get('/savings', (req, res) => {
-  res.render('account', { accounts: accounts.savings });
+  res.render('account', { account: accounts.savings });
 });
 
 app.get('/checking', (req, res) => {
