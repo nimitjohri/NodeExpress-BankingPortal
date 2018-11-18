f61c54fbb3c0f8ce40a047bfe7e44fb7328504e7
nimitjohri <nimitjohri5@gmail.com>
2018-11-18 23:26:33 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -45,8 +45,7 @@ app.post('/transfer', (req, res) => {
 })
 
 app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
-app.post('/payment', (req, res) => {
-  accounts.credit.balance -= req.body.amount;
+app.post('/payment', (req, res) => {  accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt( req.body.amount, 10);
   const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'),accountsJSON,
